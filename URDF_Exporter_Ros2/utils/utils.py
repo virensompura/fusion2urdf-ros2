e511566881808d761daa8dbb59e4dca835a55ad8
virensompura <100958586+virensompura@users.noreply.github.com>
2024-09-24 23:31:47 +0530

    Update

--- URDF_Exporter_Ros2/utils/utils.py
+++ URDF_Exporter_Ros2/utils/utils.py
@@ -9,7 +9,7 @@ import adsk, adsk.core, adsk.fusion
 import os.path, re
 from xml.etree import ElementTree
 from xml.dom import minidom
-from distutils.dir_util import copy_tree
+from shutil import copytree
 import fileinput
 import sys
 
@@ -167,7 +167,7 @@ def create_package(package_name, save_dir, package_dir):
     try: os.mkdir(save_dir + '/test')
     except: pass
 
-    copy_tree(package_dir, save_dir)
+    copytree(package_dir, save_dir, dirs_exist_ok=True)
 
 def update_setup_py(save_dir, package_name):
     file_name = save_dir + '/setup.py'
